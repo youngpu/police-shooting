3b6fc168befe9a737721ebda1a415a66b4f1ce46
Michael Freeman <michael.k.freeman@gmail.com>
2015-06-10 14:16:35 -0700

    Init commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Police shootings
+
+This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/police-shooting).
---
+++ js/map.js
@@ -0,0 +1,32 @@
+// Function to draw your map
+var drawMap() = function() {
+
+  // Create map and set viewd
+ 
+
+  // Create an tile layer variable using the appropriate url
+
+  // Add the layer to your map
+ 
+
+  // Execute your function to get data
+ 
+}
+
+// Function for getting data
+var getData = function() {
+
+  // Execute an AJAX request to get the data in data/response.js
+
+
+  // When your request is successful, call your customBuild function
+
+}
+
+// Do something creative with the data here!  
+var customBuild = function() {
+
+  
+}
+
+
